347759e81813e11cabdb64dd234164982ea8d461
MRJ2812 <mdjoy2812@gmail.com>
2023-09-25 15:23:07 +0600

    25/9

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ var cors = require('cors')
 const port = process.env.PORT || 4000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
+// Midddleware
 app.use(cors());
 app.use(express.json());
 
+// DOTENV file
+require('dotenv').config()
 
-// FinalProject
-// AuQ1rTI01n3DWsIR
 
 // This code is provided by mongoDB, when create a new connection.
-const uri = "mongodb+srv://Mongo-recheck-1:xOBlO8SEWHGJMTkM@cluster0.swhr3qz.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.swhr3qz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
@@ -22,70 +23,87 @@ async function run() {
         await client.connect();
 
         // Give website name as db name. And store data as collection name;
-        const user1 = client.db("Serene").collection("Products_Data");
+        const Products_Data = client.db("Serene").collection("Products_Data");
 
 
         // Find all the data "{}"; When we get the file we have to convert it to Array.
-        // app.get('/users', async (req, res) => {
-        //     const query = {}
-        //     const cursor = user1.find(query);
-        //     const users = await cursor.toArray();
-        //     res.send(users);
-        // })
-
-
-        // get specific data
-        // app.get('/user/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) };
-        //     const user = await user1.findOne(query);
-        //     res.send(user);
-        // })
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = Products_Data.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        })
 
         // Inset data into the database.
-        // app.post('/users', async (req, res) => {
-        //     const user = req.body;
-        //     const result = await user1.insertOne(user);
-        //     res.send(result);
-        // })
+        app.post('/product', async (req, res) => {
+            const user = req.body;
+            const result = await Products_Data.insertOne(user);
+            res.send(result);
+        })
+
+
+        // get specific product data.
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const user = await Products_Data.findOne(query);
+            res.send(user);
+        })
+
+        // Filter product with query (email)
+        // orders?email=mdjoy@gmail.com (if we need find something without id)
+        app.get('/myProduct', async (req, res) => {
+            let query = {};
+            if (req.query.sellerEmail) {
+                query = {
+                    sellerEmail: req.query.sellerEmail
+                }
+            }
+            const cursor = Products_Data.find(query);
+            const product = await cursor.toArray();
+            res.send(product);
+        });
+
+
 
         // Update user info.
         // upsert means if user not found then insert one. (sometimes no need upsert)
-        // app.put('/users/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const filter = { _id: new ObjectId(id) };
-        //     const user = req.body;
-        //     const options = { upsert: true };
-        //     const updateDoc = {
-        //         $set: {
-        //             name: user.name,
-        //             email: user.email
-        //         },
-        //     };
-        //     const result = await user1.updateOne(filter, updateDoc, options);
-        //     res.send(result);
-        // })
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: new ObjectId(id) };
+            const product = req.body;
 
-        // app.delete('/users/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: new ObjectId(id) };
-        //     const result = await user1.deleteOne(query);
-        //     res.send(result);
-        // })
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    category: product.category,
+                    name: product.name,
+                    sellerName: product.sellerName,
+                    sellerEmail: product.sellerEmail,
+                    ratings: product.ratings,
+                    ratingsCount: product.ratingsCount,
+                    img: product.img,
+                    quantity: product.quantity,
+                    price: product.price,
+                    stock: product.stock,
+                    shipping: product.shipping,
+                    description: product.description
+                },
+            };
+            console.log(updateDoc);
+            const result = await Products_Data.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
 
-        // This is query 
-        // orders?email=mdjoy@gmail.com (if we need find something without id)
-        // app.get('/orders', async (req, res) => {
-        //     let query = {};
-        //     if (req.query.email) {
-        //         query = {
-        //             email: req.query.email
-        //         }
-        //     }
-        //     const cursor = Orders.find(query);
-        //     const orders = await cursor.toArray();
-        //     res.send(orders);
-        // });
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await Products_Data.deleteOne(query);
+            res.send(result);
+        })
+
+
 
     } finally {
         // We don't need to close the connnection.
